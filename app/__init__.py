cc7d2f20561d03aa9cafd137879e3af424e14764
Stanley Hoo <shoo50@stuy.edu>
2024-10-29 10:59:28 -0400

    homepage

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,11 @@ import os
 
 app = Flask(__name__)
 
+# Home route
+@app.route('/')
+def home():
+    return render_template('home.html')
+
 if __name__ == "__main__":
     app.debug = True
     app.run()
