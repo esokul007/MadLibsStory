86a1610eb5a70716f3ad14ece99bfb5d0eb43eef
Stanley Hoo <shoo50@stuy.edu>
2024-10-29 10:49:03 -0400

    created files

---
+++ app/__init__.py
@@ -0,0 +1,15 @@
+# Stanley Hoo, Jacob Lukose, Naomi Lai, Colyi Chen
+# Indigo
+# SoftDev
+# P00: Move Slowly and Fix Things
+# 2024-10-28
+# Time spent: XXXXXXX
+
+from flask import Flask, request, render_template, make_response, redirect, url_for
+import os
+
+app = Flask(__name__)
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
