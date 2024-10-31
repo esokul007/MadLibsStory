47cf3970f1c9208fa9d38564679d693b275e2262
Stanley Hoo <shoo50@stuy.edu>
2024-10-31 10:49:45 -0400

    start

--- app/database.py
+++ app/database.py
@@ -1,7 +1,13 @@
+# Stanley Hoo, Jacob Lukose, Naomi Lai, Colyi Chen
+# Indigo
+# SoftDev
+# P00: Move Slowly and Fix Things
+# 2024-10-28
+# Time spent: XXXXXXX
+
 from flask import Flask, request, jsonify, render_template, redirect, url_for, flash, session
 from flask_cors import CORS
 import numpy as np
-from sklearn.ensemble import RandomForestRegressor
 import sqlite3
 import os
 from werkzeug.security import generate_password_hash, check_password_hash
---
+++ app/render.py
@@ -0,0 +1,14 @@
+# Stanley Hoo, Jacob Lukose, Naomi Lai, Colyi Chen
+# Indigo
+# SoftDev
+# P00: Move Slowly and Fix Things
+# 2024-10-28
+# Time spent: XXXXXXX
+
+from flask import Flask, request, jsonify, render_template, redirect, url_for, flash, session
+from flask_cors import CORS
+import numpy as np
+import sqlite3
+import os
+from werkzeug.security import generate_password_hash, check_password_hash
+import json
