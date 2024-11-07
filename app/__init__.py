3c89add72456f2071cee0792ede65e5c57b4d103
stanleyhoo1 <stanleyhoo0427@gmail.com>
2024-11-07 02:19:15 -0500

    finished project

--- app/__init__.py
+++ app/__init__.py
@@ -5,41 +5,29 @@
 # 2024-10-28
 # Time spent: 7
 
+# Imports
 from flask import Flask, request, render_template, redirect, url_for, flash, session
 import os
 from database import create_user, login_user, logout_user, create_story, create_edit, get_stories, can_add_to_story, add_to_story, get_contributors
 
+# Auto-generated secret key
 app = Flask(__name__)
 app.secret_key = os.environ.get('SECRET_KEY') or os.urandom(32)
 
-# Home route
-@app.route('/')
-def home():
-    content = display_stories()
-    
-    if 'username' in session:
-        username = session['username']
-        return render_template('home.html', content=content, username=username)
-    return render_template('home.html', content=content)
-
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    if request.method == 'POST':
-        create_user()
-        return redirect(url_for('home'))
-    return render_template('register.html')
-
+# Login route
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         login_user()
     return redirect(url_for('home'))
 
+# Logout route
 @app.route('/logout', methods=['GET', 'POST'])
 def logout():
     logout_user()
     return redirect(url_for('home'))
 
+# Create route
 @app.route('/create', methods=['GET', 'POST'])
 def create_new():
     if 'username' not in session:
@@ -51,7 +39,8 @@ def create_new():
             if created:
                 return redirect(url_for('home'))
         return render_template('create_story.html')
-    
+
+# Edit route (verifying if they can edit)
 @app.route('/edit', methods=['GET', 'POST'])
 def edit():
     if 'username' not in session:
@@ -59,27 +48,43 @@ def edit():
         return redirect(url_for('home'))
     else:
         if request.method == 'POST':
-            story_id = request.form.get('data-id')
-            add = can_add_to_story(story_id)
-            if add:
-                edit_story(story_id)
-                return render_template('edit.html')
+            # Gets stored info
+            story_id = request.form.get('story_id')
+            previous_edit = request.form.get('previous_edit')
+            # Line breaks for text
+            while '\n' in previous_edit:
+                previous_edit = previous_edit.replace('\n', '<br><br>')
+            add, completed = can_add_to_story(story_id)
+            # Checks if story is closed
+            if completed == True:
+                flash('Story is closed to new edits! It is complete!', 'error')
+                return redirect(url_for('home'))
+            # Checks if user can add to the story (ie. they already edited it)
+            elif add:
+                edit_story(story_id, previous_edit)
+                return render_template('edit.html', story_id=story_id, previous_edit=previous_edit)
+            # They can make edits
             else:
                 flash('You have already edited this story!', 'error')
                 return redirect(url_for('home'))
 
-def edit_story(story_id):
+# Function to store the edited story info in the session so it doesn't lose the info when reloading
+def edit_story(story_id, previous_edit):
     session['story_id'] = story_id  # Store story_id in the session
+    session['previous_edit'] = previous_edit
 
+# Route to add edits (actual edit)
 @app.route('/add', methods=['GET', 'POST'])
 def add():
+    # Redirects to home if not logged in (maybe they saved think somewhere to access)
     if 'username' not in session:
         flash('You must be logged in to add to a story!', 'error')
         return redirect(url_for('home'))
     else:
         story_id = session.get('story_id')  # Retrieve story_id from session
         if request.method == 'POST':
-            added = add_to_story(story_id)  # Pass story_id to the function
+            completed = True if 'toggleComplete' in request.form else False
+            added = add_to_story(story_id, completed)  # Pass story_id to the function
             if added:
                 flash('You have successfully added to the story!', 'success')
                 session.pop('story_id', None)
@@ -87,48 +92,120 @@ def add():
             else:
                 flash('You have already edited this story!', 'error')
                 return redirect(url_for('home'))
-        return render_template('edit.html', story_id=story_id)
-    
-def display_stories():
-    pairs, edits, user_edits = get_stories()
-    # print(pairs, edits, user_edits)
+        return render_template('edit.html', story_id=story_id, previous_edit=previous_edit)
+
+
+# Creates the html that will display the stories on the webpage
+def display_stories(info_type):
+    pairs, edits, user_edits = get_stories(info_type)
     text = ''''''
+    # Goes though every single returned story
     for pair in pairs:
         text += '<div style="background-color: #fffbf6;'
         text += 'padding: 5px 10px 10px 10px;'
         text += 'border-radius: 15px;'
         text += '">'
-        title, story_id = pair
+        title, story_id, completed = pair
+        
+        # Contributors list
         contributor_list = get_contributors(story_id)
         contributors = ""
         for i in contributor_list:
             contributors += i + ", "
         contributors = contributors[:-2]
         
-        if story_id in user_edits:
+        # Displays full story
+        if story_id in user_edits or completed == True:
             story_text = ""
             counter = 1
             for edit in edits[story_id]:
-                story_text += f'<p>Edit {counter}: <br>{edit}</p>\n'
+                original = edit
+                while '\n' in edit:
+                    edit = edit.replace('\n', '<br><br>')
+                story_text += f'<p>{edit}</p>\n'
                 counter += 1
+        # Preview of story
         else:
             story_text = edits[story_id][-1]
             last_edit = len(edits[story_id])
+            original = story_text
+            # New line for all new lines otherwise its all 1 line
+            while '\n' in story_text:
+                story_text = story_text.replace('\n', '<br><br>')
             
-        text += f'<h3>{title}</h3>\n'
-        if story_id in user_edits:
+        # Story title
+        text += f'<h3>Title: {title}</h3>\n'
+        # Story status
+        status = 'Completed' if completed == True else 'In progress'
+        text += f'<b>Status: {status}</b>\n'
+        # Actual story
+        if story_id in user_edits or completed == True:
             text += f'<p>{story_text}</p>\n'
         else:
-            text += f'<p>Edit {last_edit}: <br>{story_text}</p>\n'
+            text += f'<p><b>Previous contribution: </b><br><br>{story_text}</p>\n'
         text += f'<p><b>Contributors:</b> {contributors}</p>'
-        text += f'''
-        <form action="{url_for('edit')}" method="POST">
-            <button type="submit" class="add-btn" name="data-id" value={story_id}>Add to Story</button>
-        </form>
-        '''
+        # If story in progress display the add to story button below
+        if completed != True:
+            text += f'''
+            <form action="{ url_for('edit') }" method="POST">
+            
+                <!-- Hidden inputs for story_id and story_text -->
+                <input type="hidden" name="story_id" value="{ story_id }">
+                <textarea name="previous_edit" style="display:none;">{ original }</textarea>
+
+                <!-- Submit button to add to story -->
+                <button type="submit" class="add-btn">Add to Story</button>
+            </form>
+            '''
         text += '</div>'
         text += '\n<br>'
     return text
+
+# Main Page Routes
+
+# Home route
+@app.route('/')
+def home():
+    content = display_stories(0)
+    if 'username' in session:
+        username = session['username']
+        return render_template('home.html', content=content, username=username)
+    return render_template('home.html', content=content)
+
+# In progress route
+@app.route('/in-progress')
+def progress():
+    content = display_stories(1)
+    if 'username' in session:
+        username = session['username']
+        return render_template('in-progress.html', content=content, username=username)
+    return render_template('in-progress.html', content=content)
+
+# Contributions route
+@app.route('/contributions')
+def contributions():
+    content = display_stories(2)
+    if 'username' in session:
+        username = session['username']
+        return render_template('contributions.html', content=content, username=username)
+    return render_template('contributions.html', content=content)
+
+# Completed route
+@app.route('/completed')
+def completed():
+    content = display_stories(3)
+    if 'username' in session:
+        username = session['username']
+        return render_template('completed.html', content=content, username=username)
+    return render_template('completed.html', content=content)
+
+# Registration route
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        create_user()
+        return redirect(url_for('home'))
+    return render_template('register.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
--- app/database.py
+++ app/database.py
@@ -58,6 +58,7 @@ def login_user():
         user_password_hash = cur.fetchone()
         conn.close()
 
+        # Checks hashed user password against database
         if user_password_hash and check_password_hash(user_password_hash['password_hash'], password):
             session['username'] = username
             flash('Login successful!', 'success')
@@ -86,7 +87,8 @@ def create_story():
         cur.execute('''
             CREATE TABLE IF NOT EXISTS stories (
                 story_id INTEGER PRIMARY KEY AUTOINCREMENT,
-                story_title TEXT NOT NULL UNIQUE
+                story_title TEXT NOT NULL UNIQUE,
+                completed BOOLEAN DEFAULT False
             );
         ''')
 
@@ -144,15 +146,44 @@ def validate_login(username, password):
     else:
         return False
 
-def get_stories():
+# Returns different story for each type of info is wanted (ie. only completed stories for 3, only user edited stories for 2, etc.)
+def get_stories(info_type):
     conn = get_db_connection()
     cur = conn.cursor()
-    result = cur.execute("SELECT story_title, story_id FROM stories").fetchall()
+    # Creates tables if they don't exist so I don't get errors when running with new database
+    cur.execute('''
+                CREATE TABLE IF NOT EXISTS users (
+                    user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    username TEXT NOT NULL UNIQUE,
+                    password_hash TEXT NOT NULL
+                );
+            ''')
+    cur.execute('''
+            CREATE TABLE IF NOT EXISTS stories (
+                story_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                story_title TEXT NOT NULL UNIQUE,
+                completed BOOLEAN DEFAULT False
+            );
+        ''')
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS story_edits (
+            edit_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_id INTEGER NOT NULL,
+            story_id INTEGER NOT NULL,
+            edit TEXT NOT NULL
+        );
+    ''')
+    # commit changes if they aren't tables
+    conn.commit()
+    
+    # Retrieves all story info
+    result = cur.execute("SELECT story_title, story_id, completed FROM stories").fetchall()
     
     # Gets the story titles and ids
     all_stories = [row['story_title'] for row in result]
     all_ids = [row['story_id'] for row in result]
-    all_story_id_pairs = ((all_stories[i], all_ids[i]) for i in range(len(all_stories)))
+    all_statuses = [row['completed'] for row in result]
+    all_story_id_pairs = [(all_stories[i], all_ids[i], all_statuses[i]) for i in range(len(all_stories))]
     
     # Checks if user is logged in, will return different info
     if 'username' in session:
@@ -175,8 +206,33 @@ def get_stories():
         if all_user_ids[i] == user_id:
             user_edited_stories.append(all_story_ids[i])
     conn.close()
-    return all_story_id_pairs, story_edits, user_edited_stories
-        
+    
+    # All stories
+    if info_type == 0:
+        return all_story_id_pairs, story_edits, user_edited_stories
+    # Stories in progress
+    elif info_type == 1:
+        new_pairs = []
+        for story_pair in all_story_id_pairs:
+            if story_pair[2] != True:
+                new_pairs.append(story_pair)
+        return new_pairs, story_edits, user_edited_stories
+    # User edited stories
+    elif info_type == 2:
+        new_pairs = []
+        for story_pair in all_story_id_pairs:
+            if story_pair[1] in user_edited_stories:
+                new_pairs.append(story_pair)
+        return new_pairs, story_edits, user_edited_stories
+    # Completed stories
+    else:
+        new_pairs = []
+        for story_pair in all_story_id_pairs:
+            if story_pair[2] == True:
+                new_pairs.append(story_pair)
+        return new_pairs, story_edits, user_edited_stories
+
+# Determines whether or not user can add to the story
 def can_add_to_story(story_id):
     if 'username' in session:
         username = session['username']
@@ -185,26 +241,35 @@ def can_add_to_story(story_id):
         user_id = cur.execute("SELECT user_id FROM users WHERE username = ?", (username,)).fetchone()[0]
         result = cur.execute("SELECT user_id FROM story_edits WHERE story_id = ?", (story_id,)).fetchall()
         all_user_edit_ids = [row['user_id'] for row in result]
+        completed = cur.execute("SELECT completed from stories WHERE story_id = ?", (story_id,)).fetchone()[0]
         conn.close()
         if user_id in all_user_edit_ids:
-            return False
-        return True
+            return False, completed
+        return True, completed
 
-def add_to_story(story_id):
+# Function to actually add to the story, along with updated status
+def add_to_story(story_id, completed):
     edit = request.form['edit']
     if can_add_to_story(story_id):
         conn = get_db_connection()
         cur = conn.cursor()
+        last_completed = cur.execute("SELECT completed from stories WHERE story_id = ?", (story_id,)).fetchone()[0]
+        if last_completed == True:
+            flash('You have already edited this story!', 'error')
+            return False
         if 'username' in session:
             username = session['username']
             user_id = cur.execute("SELECT user_id FROM users WHERE username = ?", (username,)).fetchone()[0]
         story_title = cur.execute("SELECT story_title FROM stories WHERE story_id = ?", (story_id,)).fetchone()[0]
         create_edit(user_id, story_title, edit)
+        cur.execute("UPDATE stories SET completed = ? WHERE story_id = ?", (completed, story_id))
+        conn.commit()
         conn.close()
         return True
     else:
         return False
         
+# Returns the contributors for a specific story
 def get_contributors(story_id):
     conn = get_db_connection()
     cur = conn.cursor()
